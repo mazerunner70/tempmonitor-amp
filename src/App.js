e70906af9b255ab0824648d370e5ed59e04ab820
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2021-08-30 19:46:23 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 const initialFormState = { name: '', description: '' }
 
 const apiName = 'sensors';
-const path = 'https://m6t068kzie.execute-api.eu-west-1.amazonaws.com/staging/sensors'; 
+// const path = 'https://m6t068kzie.execute-api.eu-west-1.amazonaws.com/staging/sensors'; 
+const path = '/sensors'; 
 // const myInit = { // OPTIONAL
 //     headers: {}, // OPTIONAL
 //     response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
@@ -25,13 +26,17 @@ function App() {
     API
       .get(apiName, path, myInit)
       .then(response => {
-        // Add your code here
-      })
+        console.log("got here");
+        console.log(response)
+          })
       .catch(error => {
+        console.log("some type of error")
+        console.log(error)
         console.log(error.response);
     }); 
+    console.log("here1")
   }, []);
-
+  console.log("here2")
 
   // async function createNote() {
   //   await API.graphql({ query: createNoteMutation, variables: { input: formData } });
